3697a985dd708daf6579435b18b6b175a3d1688b
David Michael Weis <dweis7@gmail.com>
2020-07-11 22:37:57 +0100

    Add arm driver

---
+++ src/arm_driver.rs
@@ -0,0 +1,30 @@
+use crate::arm_config;
+use async_trait::async_trait;
+use lss_driver;
+use std::error::Error;
+
+#[async_trait(?Send)]
+pub trait ArmDriver {
+    async fn set_color(&mut self, color: lss_driver::LedColor) -> Result<(), Box<dyn Error>>;
+}
+
+pub struct SerialArmDriver {
+    driver: lss_driver::LSSDriver,
+}
+
+impl SerialArmDriver {
+    pub fn new(port: &str) -> Result<Box<dyn ArmDriver>, Box<dyn Error>> {
+        let driver = lss_driver::LSSDriver::new(port)?;
+        Ok(Box::new(SerialArmDriver { driver }))
+    }
+}
+
+#[async_trait(?Send)]
+impl ArmDriver for SerialArmDriver {
+    async fn set_color(&mut self, color: lss_driver::LedColor) -> Result<(), Box<dyn Error>> {
+        self.driver
+            .set_color(lss_driver::BROADCAST_ID, color)
+            .await?;
+        Ok(())
+    }
+}
