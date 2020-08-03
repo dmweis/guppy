d978ba35fb708d3cfc152215a6658e5f57ca64f0
David Weis <dweis7@gmail.com>
2020-08-03 04:34:08 +0100

    Add teach pendent and config move

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod speech_service;
 #[cfg(feature = "visualiser")]
 mod visualizer;
 
+
+use async_std::io;
 use async_std::task::sleep;
 use std::time::Duration;
 #[cfg(feature = "visualiser")]
@@ -16,6 +18,8 @@ use nalgebra as na;
 use std::path::Path;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
+use std::time::Instant;
+
 
 #[derive(Clap)]
 #[clap()]
@@ -29,6 +33,8 @@ enum SubCommand {
     DisplayPositions(DisplayPositionsArgs),
     Ik(GenericArgs),
     Move(GenericArgs),
+    MoveConfig(GenericArgs),
+    TeachPendent(GenericArgs),
     Config(ConfigArgs),
     #[cfg(feature = "visualiser")]
     Viz,
@@ -71,8 +77,14 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         SubCommand::Ik(args) => {
             ik_run(args).await?;
         }
+        SubCommand::TeachPendent(args) => {
+            teach_pendent(args).await?;
+        }
         SubCommand::Move(args) => {
             move_run(args).await?;
+        }
+        SubCommand::MoveConfig(args) => {
+            move_config_run(args).await?;
         }
         #[cfg(feature = "visualiser")]
         SubCommand::Viz => test_visualizer().await?,
@@ -167,7 +179,7 @@ async fn ik_run(args: GenericArgs) -> Result<(), Box<dyn std::error::Error>> {
                 let translated_fk = arm_controller.calculate_fk(calculated_ik).await?;
                 visualizer.set_motion_plan(Some(vec![translated_fk]));
             }
-            sleep(Duration::from_micros(20)).await;
+            sleep(Duration::from_millis(20)).await;
         } else {
             eprintln!("Message error");
         }
@@ -222,12 +234,155 @@ async fn move_run(args: GenericArgs) -> Result<(), Box<dyn std::error::Error>> {
         } else {
             eprintln!("Message error");
         }
-        sleep(Duration::from_micros(20)).await;
+        sleep(Duration::from_millis(20)).await;
     }
     arm_controller.limp().await?;
     arm_controller
         .set_color(lss_driver::LedColor::Yellow)
         .await?;
+    sleep(Duration::from_secs_f32(0.5)).await;
+    Ok(())
+}
+
+async fn move_config_run(args: GenericArgs) -> Result<(), Box<dyn std::error::Error>> {
+    let running = Arc::new(AtomicBool::new(true));
+    let running_handle = running.clone();
+    let desired_point = Arc::new(Mutex::new(EndEffectorPose::new(
+        na::Vector3::new(0.2, 0., 0.2),
+        0.,
+    )));
+    #[cfg(feature = "visualiser")]
+    let mut visualizer = VisualizerInterface::new(desired_point.clone());
+
+    let mut settings = arm_driver::ArmControlSettings::default();
+    settings.save_json("./motor_settings.json")?;
+
+    ctrlc::set_handler(move || {
+        running_handle.store(false, Ordering::Release);
+        println!("Caught interrupt\nExiting...");
+    })?;
+
+    if args.speak {
+        speech_service::say(format!("Waking up arm. Connecting to {}", args.port)).await?;
+    }
+    let mut driver =
+        arm_driver::SerialArmDriver::new(&args.port, arm_config::ArmConfig::included()).await?;
+    driver.set_color(lss_driver::LedColor::Magenta).await?;
+    let mut arm_controller =
+        arm_controller::LssArmController::new(driver, arm_config::ArmConfig::included());
+
+    if args.speak {
+        speech_service::say("Connected successfully!".to_owned()).await?;
+    }
+    let start = Instant::now();
+    let mut last_reload = Instant::now();
+    while running.load(Ordering::Acquire) {
+        let temporal = (start.elapsed().as_secs_f32() * 2.).sin();
+        let z = temporal * 0.07;
+        let y = temporal * 0.07;
+        let position = na::Vector3::new(0.2, 0.0 + y, 0.2 + z);
+        if let Ok(_arm_positions) = arm_controller
+            .move_to(position, 0.0)
+            .await
+        {
+            #[cfg(feature = "visualiser")]
+            {
+                let joint_positions = arm_controller.calculate_fk(_arm_positions).await?;
+                visualizer.set_position(joint_positions);
+            }
+        } else {
+            eprintln!("Message error");
+        }
+        sleep(Duration::from_millis(20)).await;
+        if last_reload.elapsed() > Duration::from_secs(4) {
+            settings = arm_driver::ArmControlSettings::load_json("./motor_settings.json")?;
+            arm_controller.setup_motors(settings).await?;
+            last_reload = Instant::now();
+            println!("Reloaded settings");
+        }
+    }
+    arm_controller.limp().await?;
+    arm_controller
+        .set_color(lss_driver::LedColor::Yellow)
+        .await?;
+    sleep(Duration::from_secs_f32(0.5)).await;
+    Ok(())
+}
+
+async fn wait_for_enter() -> Result<(), Box<dyn std::error::Error>> {
+    let stdin = io::stdin();
+    let mut line = String::new();
+    stdin.read_line(&mut line).await?;
+    Ok(())
+}
+
+async fn teach_pendent(args: GenericArgs) -> Result<(), Box<dyn std::error::Error>> {
+    let running = Arc::new(AtomicBool::new(true));
+    let running_handle = running.clone();
+    let desired_point = Arc::new(Mutex::new(EndEffectorPose::new(
+        na::Vector3::new(0.2, 0., 0.2),
+        0.,
+    )));
+    #[cfg(feature = "visualiser")]
+    let mut visualizer = VisualizerInterface::new(desired_point.clone());
+
+    ctrlc::set_handler(move || {
+        running_handle.store(false, Ordering::SeqCst);
+        println!("Caught interrupt\nExiting...");
+    })?;
+
+    if args.speak {
+        speech_service::say(format!("Waking up arm. Connecting to {}", args.port)).await?;
+    }
+    let driver = arm_driver::SerialArmDriver::new(&args.port, arm_config::ArmConfig::included()).await?;
+    let mut arm_controller = arm_controller::LssArmController::new(driver, arm_config::ArmConfig::included());
+        
+    if args.speak {
+        speech_service::say("Connected successfully!".to_owned()).await?;
+    }
+        
+    let mut history = vec![];
+
+    arm_controller.set_color(lss_driver::LedColor::Magenta).await?;
+    arm_controller.limp().await?;
+
+    println!("Starting recording\npress Ctrl+C to exit record mode");
+    while running.load(Ordering::SeqCst) {
+        println!("Press enter to record position");
+        wait_for_enter().await?;
+        let current_position = arm_controller.read_position().await?;
+        history.push(current_position.clone());
+        #[cfg(feature = "visualiser")]
+        visualizer.set_position(current_position);
+    }
+    
+    println!("Entering replay mode\npress Ctrl+c to exit");
+    arm_controller.set_color(lss_driver::LedColor::Cyan).await?;
+    running.store(true, Ordering::SeqCst);
+
+    while running.load(Ordering::SeqCst) {
+        for arm_pose in &history {
+            if let Ok(_arm_positions) = arm_controller.move_to(arm_pose.end_effector, arm_pose.end_effector_angle).await
+            {
+                #[cfg(feature = "visualiser")]
+                {
+                    let joint_positions = arm_controller.calculate_fk(_arm_positions).await?;
+                    visualizer.set_position(joint_positions);
+                }
+            } else {
+                eprintln!("Message error");
+            }
+            sleep(Duration::from_secs(1)).await;
+        }
+        println!("Sequence done\nPress enter to repeat");
+        wait_for_enter().await?;
+        sleep(Duration::from_millis(500)).await;
+        if !running.load(Ordering::SeqCst) {
+            break;
+        }
+    }
+    arm_controller.set_color(lss_driver::LedColor::Red).await?;
+    arm_controller.limp().await?;
     sleep(Duration::from_secs_f32(0.5)).await;
     Ok(())
 }
