9e6453809b886a9a8fc11a95ebdb480293e11619
dmweis <dmweis@users.noreply.github.com>
2021-02-28 00:28:59 +0000

    deploy: b43534258fb9db8a651dd4bfc0386995acc87c01

--- source-files.js
+++ source-files.js
@@ -2,5 +2,5 @@ var N = null;var sourcesIndex = {};
 sourcesIndex["cli"] = {"name":"","files":["cli.rs"]};
 sourcesIndex["grpc"] = {"name":"","files":["grpc.rs"]};
 sourcesIndex["gui_remote"] = {"name":"","files":["gui_remote.rs"]};
-sourcesIndex["guppy"] = {"name":"","files":["arm_config.rs","arm_controller.rs","arm_driver.rs","grpc_controller.rs","lib.rs","speech_service.rs","visualizer.rs"]};
+sourcesIndex["guppy"] = {"name":"","files":["arm_config.rs","arm_controller.rs","arm_driver.rs","grpc_controller.rs","lib.rs","visualizer.rs"]};
 createSourceSidebar();
