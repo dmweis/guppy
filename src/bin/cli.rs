efc81f2f79be1fbe4cf40973bda8f767df6630df
David Weis <dweis7@gmail.com>
2021-02-28 00:50:24 +0000

    Remove unused endpoint and add comments

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -10,7 +10,6 @@ use nalgebra as na;
 use std::sync::atomic::{AtomicBool, Ordering};
 use std::sync::{Arc, Mutex};
 use std::time::Duration;
-use std::time::Instant;
 
 #[derive(Clap)]
 #[clap()]
@@ -24,7 +23,6 @@ enum SubCommand {
     DisplayPositions(GenericArgs),
     Ik(GenericArgs),
     Move(GenericArgs),
-    MoveConfig(GenericArgs),
     TeachPendent(GenericArgs),
     Viz,
 }
@@ -50,9 +48,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         }
         SubCommand::Move(args) => {
             move_run(args).await?;
-        }
-        SubCommand::MoveConfig(args) => {
-            move_config_run(args).await?;
         }
         SubCommand::Viz => test_visualizer().await?,
     }
@@ -99,6 +94,7 @@ async fn test_visualizer() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+/// Monitor pose in loop and display
 async fn ik_run(args: GenericArgs) -> Result<(), Box<dyn std::error::Error>> {
     let running = Arc::new(AtomicBool::new(true));
     let running_handle = running.clone();
@@ -192,59 +188,6 @@ async fn move_run(args: GenericArgs) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-async fn move_config_run(args: GenericArgs) -> Result<(), Box<dyn std::error::Error>> {
-    let running = Arc::new(AtomicBool::new(true));
-    let running_handle = running.clone();
-    let desired_point = Arc::new(Mutex::new(EndEffectorPose::new(
-        na::Vector3::new(0.2, 0., 0.2),
-        0.,
-    )));
-    let mut visualizer =
-        VisualizerInterface::new(desired_point.clone(), Arc::new(AtomicBool::new(true)));
-
-    let mut settings = arm_driver::ArmControlSettings::default();
-    settings.save_json("./motor_settings.json")?;
-
-    ctrlc::set_handler(move || {
-        running_handle.store(false, Ordering::Release);
-        println!("Caught interrupt\nExiting...");
-    })?;
-
-    let mut driver =
-        arm_driver::SerialArmDriver::new(&args.port, arm_config::ArmConfig::included()).await?;
-    driver.set_color(lss_driver::LedColor::Magenta).await?;
-    let mut arm_controller =
-        arm_controller::LssArmController::new(driver, arm_config::ArmConfig::included());
-
-    let start = Instant::now();
-    let mut last_reload = Instant::now();
-    while running.load(Ordering::Acquire) {
-        let temporal = (start.elapsed().as_secs_f32() * 2.).sin();
-        let z = temporal * 0.07;
-        let y = temporal * 0.07;
-        let position = na::Vector3::new(0.2, 0.0 + y, 0.2 + z);
-        if let Ok(_arm_positions) = arm_controller.move_to(position, 0.0).await {
-            let joint_positions = arm_controller.calculate_fk(_arm_positions).await?;
-            visualizer.set_position(joint_positions);
-        } else {
-            eprintln!("Message error");
-        }
-        sleep(Duration::from_millis(20)).await;
-        if last_reload.elapsed() > Duration::from_secs(4) {
-            settings = arm_driver::ArmControlSettings::load_json("./motor_settings.json")?;
-            arm_controller.setup_motors(settings).await?;
-            last_reload = Instant::now();
-            println!("Reloaded settings");
-        }
-    }
-    arm_controller.limp().await?;
-    arm_controller
-        .set_color(lss_driver::LedColor::Yellow)
-        .await?;
-    sleep(Duration::from_secs_f32(0.5)).await;
-    Ok(())
-}
-
 async fn wait_for_enter() -> Result<(), Box<dyn std::error::Error>> {
     let stdin = io::stdin();
     let mut line = String::new();
@@ -252,6 +195,7 @@ async fn wait_for_enter() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
+/// read pose for a bit and then repeat poses
 async fn teach_pendent(args: GenericArgs) -> Result<(), Box<dyn std::error::Error>> {
     let running = Arc::new(AtomicBool::new(true));
     let running_handle = running.clone();
