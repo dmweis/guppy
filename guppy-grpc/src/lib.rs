ce001f7a79df6bc528d34fbf31e53f90ae25a1e1
David Weis <dweis7@gmail.com>
2021-02-28 01:58:48 +0000

    Export guppy controller from crates

--- guppy-grpc/src/lib.rs
+++ guppy-grpc/src/lib.rs
@@ -1,1 +1,4 @@
 pub mod grpc_controller;
+pub use guppy_controller::arm_config;
+pub use guppy_controller::arm_controller;
+pub use guppy_controller::arm_driver;
--- guppy-ui/src/lib.rs
+++ guppy-ui/src/lib.rs
@@ -1,1 +1,4 @@
 pub mod visualizer;
+pub use guppy_controller::arm_config;
+pub use guppy_controller::arm_controller;
+pub use guppy_controller::arm_driver;
